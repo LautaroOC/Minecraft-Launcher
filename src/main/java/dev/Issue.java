38242c69451f9d2ff1f2bfd767a807f3a10a4ef0
LautaroOC <lautaroortizcapurro@gmail.com>
2026-03-09 21:19:37 -0300

    added the issueTypes enum

--- src/main/java/dev/Issue.java
+++ src/main/java/dev/Issue.java
@@ -3,24 +3,18 @@ package dev;
 import java.nio.file.Path;
 
 public class Issue {
-    private String issueName;
+    private IssueType issueType;
     private String sha1;
     private long size;
     private String url;
     private Path pathIssue;
-    private
 
     public Issue(String issueName, String sha1, long size, String url, Path pathIssue) {
-        this.issueName = issueName;
         this.sha1 = sha1;
         this.size = size;
         this.url = url;
         this.pathIssue = pathIssue;
     }
-
-   public String getIssueName() {
-       return issueName;
-   }
 
    public String getSha1() {
        return sha1;
--- src/main/java/dev/IssueType.java
+++ src/main/java/dev/IssueType.java
@@ -1,4 +1,8 @@
 package dev;
 
 public enum IssueType {
+    MISSING_FILE,
+    SIZE_MISMATCH,
+    HASH_MISMATCH,
+    MISSING_DIRECTORY
 }
